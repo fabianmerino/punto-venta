6de3a72e84ec5f2715c6c76618eff966738214ce
Fabian Merino <fab_ary@outlook.com>
2019-04-01 13:38:36 -0500

    Actualizaciones varias

---
+++ resources/assets/js/components/Inventario/KardexComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "KardexComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/pages/inventario/kardex.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fab_a
+ * Date: 31/01/2019
+ * Time: 11:37 PM
+ */
